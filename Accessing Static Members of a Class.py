5c48bbb1c096460ed76387f9f9b47d531097c4bb
Ankit Kumar <ankitsingh31711@gmail.com>
2024-11-16 21:19:56 +0530

    ..

---
+++ Accessing Static Members of a Class.py
@@ -0,0 +1,24 @@
+include <iostream>
+using namespace std;
+
+class Counter {
+private:
+    static int count; // Static variable declaration
+public:
+    static void increment() {
+        count++;
+    }
+    static int getCount() {
+        return count;
+    }
+};
+
+// Definition of static variable outside the class
+int Counter::count = 0;
+
+int main() {
+    Counter::increment(); // Access static method using ::
+    Counter::increment();
+    cout << "Count: " << Counter::getCount() << endl;
+    return 0;
+}
