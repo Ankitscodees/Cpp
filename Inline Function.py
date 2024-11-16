c670f04918b9b29d0bf009cb8a86ea078a2e7b7d
Ankit Kumar <ankitsingh31711@gmail.com>
2024-11-16 21:22:01 +0530

    ..

---
+++ Inline Function.py
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Inline function definition
+inline int square(int num) {
+    return num * num;
+}
+
+int main() {
+    int a = 5, b = 10;
+
+    // Inline function calls
+    cout << "Square of " << a << " is " << square(a) << endl;
+    cout << "Square of " << b << " is " << square(b) << endl;
+
+    return 0;
+}
