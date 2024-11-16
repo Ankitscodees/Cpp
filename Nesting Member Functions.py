a6186c6152d9339677524a92df86e0b2e815358f
Ankit Kumar <ankitsingh31711@gmail.com>
2024-11-16 21:28:27 +0530

    ..

---
+++ Nesting Member Functions.py
@@ -0,0 +1,37 @@
+include <iostream>
+using namespace std;
+
+class Calculator {
+private:
+    int number1, number2;
+
+public:
+    // Function to set the numbers
+    void setNumbers(int num1, int num2) {
+        number1 = num1;
+        number2 = num2;
+    }
+
+    // Function to calculate the sum
+    int calculateSum() {
+        return number1 + number2;
+    }
+
+    // Function to display the sum (calls calculateSum)
+    void displaySum() {
+        int sum = calculateSum(); // Nested function call
+        cout << "The sum of " << number1 << " and " << number2 << " is: " << sum << endl;
+    }
+};
+
+int main() {
+    Calculator calc;
+
+    // Setting numbers
+    calc.setNumbers(10, 20);
+
+    // Displaying the sum
+    calc.displaySum();
+
+    return 0;
+}
